d85c3b6c7b256ff8009cd2d49251056bff966fc8
Shah <info@shahnewazkhan.ca>
2017-12-20 21:56:37 -0800

    Added filters & errors

---
+++ app.js
@@ -0,0 +1,17 @@
+var Twitter = require('twitter');
+var env = require('dotenv').config();
+
+var streamFilter = require('./stream_helpers/filters');
+var streamError = require('./stream_helpers/errors');
+
+var client = new Twitter({
+    consumer_key: env.parsed.consumer_key,
+    consumer_secret: env.parsed.consumer_secret,
+    access_token_key: env.parsed.access_token_key,
+    access_token_secret: env.parsed.access_token_secret
+  });
+
+  client.stream('statuses/filter', {track: 'looking for a job in investment banking'}, function (stream) {
+    stream.on('data', streamFilter);
+    stream.on('error', streamError);
+  });
